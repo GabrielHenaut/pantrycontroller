18b6989fb5df92a448545b80e2f5ded4045c6158
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2022-01-19 17:06:57 +0000

    added Procfile

--- application.py
+++ application.py
@@ -36,7 +36,10 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///storage.db")
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
 
 
 
