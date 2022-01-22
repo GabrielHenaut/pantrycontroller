9f59daac918049834d5393ec20a0e9a5e70f5d72
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2022-01-22 05:01:28 +0000

    should be bak to the begging

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-Configure CS50 Library to use SQLite database
+# Configure CS50 Library to use SQLite database
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://")
