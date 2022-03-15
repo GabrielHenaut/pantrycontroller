4db6c38057233d15f100d62f2e62c362a6ff36d0
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2022-03-15 19:11:21 +0000

    fixed datetime but (for the 50 time), hopefully for the last time ;-; v3

--- application.py
+++ application.py
@@ -143,7 +143,7 @@ def add_item():
         storage = db.execute("SELECT item, amount, unit, expiration, type FROM itens WHERE user_id=?", session["user_id"])
         for item in storage:
             item["item"] = item["item"].title()
-            expiration = datetime.strptime(item["expiration"], "%Y-%m-%d")
+            expiration = datetime.strptime(str(item["expiration"]), "%Y-%m-%d")
             item["expiration"] = (f"{expiration.month}/{expiration.day}/{expiration.year}")
         if len(storage) < 1:
             return render_template("add_item.html", storage="")
